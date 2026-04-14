9c9a37bba1f8d0da9efc1a905da0118984581d44
brenatamellini <b.tamellini@gmail.com>
2026-04-14 16:17:36 +0100

    criando pasta services e a guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from "./components/home/home";
+import { Login } from "./components/login/login";
+import { Registo } from "./components/registo/registo";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'login', component: Login },
+    { path: 'registo', component: Registo },
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
