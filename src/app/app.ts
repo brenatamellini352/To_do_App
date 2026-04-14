335d3ba8dee011b2ae8a7faccbea7c0d3c736b55
brenatamellini <b.tamellini@gmail.com>
2026-04-14 14:58:56 +0100

    passando codigo base do registo.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./components/login/login";
+import { Registo } from "./components/registo/registo";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Login, Registo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/registo/registo.ts
+++ src/app/components/registo/registo.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registo',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterModule],
   templateUrl: './registo.html',
   styleUrl: './registo.css',
 })
-export class Registo {}
+export class Registo {
+  // Objeto para armazenar os dados do formulário
+  registoData = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
+
+  onRegisto() {
+    console.log('Dados enviados para o registo:', this.registoData);
+    
+    // validação simples so para testar
+    if (this.registoData.password !== this.registoData.confirmPassword) {
+      alert('As senhas não coincidem!');
+      return;
+    }
+  }
+}
