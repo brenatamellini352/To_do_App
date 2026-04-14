95f6e2e57abdbae2ca2a03fc12c272278207bdf4
brenatamellini <b.tamellini@gmail.com>
2026-04-14 15:46:09 +0100

    codigo base home adicionado

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./components/login/login";
 import { Registo } from "./components/registo/registo";
+import { Home } from "./components/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Registo],
+  imports: [RouterOutlet, Login, Registo, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  newTaskTitle: string = '';
+  tasks: any[] = [];
+  progressValue: number = 0;
+
+  addTask() {
+    if (this.newTaskTitle.trim()) {
+      this.tasks.push({ title: this.newTaskTitle, completed: false });
+      this.newTaskTitle = '';
+      console.log('Tarefa adicionada!', this.tasks);
+    }
+  }
+
+  toggle_theme() {
+    console.log('Alternando tema...');
+  }
+}
