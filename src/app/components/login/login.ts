8e453f08adb963b6a700292018b972bdb141c463
brenatamellini <b.tamellini@gmail.com>
2026-04-14 14:22:36 +0100

    passando codigo do login.html

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  // Objeto para armazenar os dados do formulário
+  loginData = {
+    email: '',
+    password: ''
+  };
+  onLogin() {
+    console.log('Botão de login clicado!', this.loginData);
+    // Aqui vai a lógica que estava do auth.js
+  }
+}
